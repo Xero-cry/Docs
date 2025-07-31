d0b7d90d4e2984bd16451089b61d9676afb28b6e
xero_cry <xero.ab.dev@gmail.com>
2025-07-31 06:03:43 -0600

    Se agragan y modifican lineas en git alias

--- zsh/git_alias.zsh
+++ zsh/git_alias.zsh
@@ -3,12 +3,14 @@
 ############################
 
 alias gi="git init"
-alias ga="git add"
 alias gs="git status"
+alias ga="git add"
+alias gaa="git add ."
 alias gc="git commit"
 alias gps="git push"
 alias gpl="git pull"
 alias gcl="git clone"
 alias gll="git log"
-alias gcho="git checkout --"
 alias gdif="git diff"
+alias gcho="git checkout"
+alias gbra="git branch"
