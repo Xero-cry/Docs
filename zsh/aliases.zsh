41362ed4520cc4106c56811d0155662ff1caf8ae
xero_cry <xero.ab.dev@gmail.com>
2025-08-05 03:51:17 -0600

    New update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,65 @@
 alias a='cd'
 alias nwaliaszs='add-alias-zsh.sh'
+
+#########
+###YAY###
+#########
+
+alias Y="sudo yay -S"
+
+############
+###Pacman###
+############
+
+alias P="sudo pacman -S"
+
+###########
+###Order###
+###########
+
+alias rb="sudo reboot now"
+alias sd="shutdown now"
+
+##############
+###Shorcuts###
+##############
+
+alias n="nvim"
+alias l="lsd"
+alias la="lsd -a"
+alias ll="lsd -l"
+alias lla="lsd -la"
+alias a="cd"
+alias .="cd .."
+alias md="mkdir"
+
+###########
+###CHMOD###
+###########
+
+alias X="chmod +x"
+alias R="chmod +r"
+alias W="chmod +w"
+
+###########
+###Confs###
+###########
+
+alias zsconf="n ~/.zshrc"
+alias hpconf="n ~/.config/hypr/hyprland.conf"
+alias hpbinds="n ~/.config/hypr/bindings.conf"
+alias kittyconf="n ~/.config/kitty/kitty.conf"
+alias fastfconf="n ~/.config/fastfetch/config.jsonc"
+alias woficonf="n ~/.config/wofi/config"
+alias wyconf="n ~/.config/waybar/config"
+alias wycss="n ~/.config/waybar/style.css"
+
+#################
+###Directories###
+#################
+
+alias hyprdocs="a ~/.config/hypr"
+alias waydocs="a ~/.config/waybar"
+alias conf="a ~/.config"
+
+# alias ohmyzsh="mate ~/.oh-my-zsh"
